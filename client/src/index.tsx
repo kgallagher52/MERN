7565b44ad718e02f501657ca6322ea79197d97fd
keaton <keatonrgallagher@gmail.com>
2021-03-05 21:55:32 -0700

    Set up redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux' // Accessing the store making available for nested components
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk' // Allows you to write action creators that return a function instead of an action. The thunk can be used to delay the dispatch of an action, or to dispatch only if a certain condition is met. 
+import reducers from './reducers/Index'
+
+
 import App from './App';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ client/src/reducers/Posts.js
@@ -0,0 +1,16 @@
+// 
+const initialState = []
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+
+            break;
+        case 'CREATE_ALL':
+
+            break;
+
+        default:
+            return state;
+    }
+}
